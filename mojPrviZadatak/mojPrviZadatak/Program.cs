57b4a52870ae0e6b09d87fed988d8ce30543bac1
Lovro Šverko <lovro.sverko@gmail.com>
2024-09-16 13:47:47 +0200

    Add files via upload

---
+++ mojPrviZadatak/mojPrviZadatak/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mojPrviZadatak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // deklaracija varijabli
+            int a, b, x, y;
+
+            // dodjela vrijednosti
+            a = 100;
+            b = 3;
+            x = a / b;
+            y = a % b;
+
+            string ispis = a + " dijeljeno s "
+                + b + " je " + x + " i ostatak "
+                + y;
+
+            // ispisivanje
+            Console.WriteLine(ispis);
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
